2f7daa49e4d9c9f95ccf3325b114e25520b361d4
Rouan Thompson <rouan.thompson@gmail.com>
2020-09-21 13:04:16 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Login from './components/Login'
+import NavBar from './components/NavBar'
+import MyProfile from './components/MyProfile'
 
 function App() {
   return (
-    <div>
-      My App
-    </div>
-  );
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/myprofile" component={MyProfile} />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
 
 // import './App.css';
---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const link = {
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    background: 'gray',
+    textDecoration: 'none',
+    color: 'white'
+}
+
+class NavBar extends React.Component{
+    render(){
+        return(
+            <div>
+                <NavLink
+                    to="/"
+                    exact
+                    style={link}
+                    activeStyle={{background: 'pink'}}
+                >Home</NavLink>
+                <NavLink
+                    to="/myprofile"
+                    exact
+                    style={link}
+                    activeStyle={{background: 'pink'}}
+                >My Profile</NavLink>
+                <NavLink
+                    to="/login"
+                    exact
+                    style={link}
+                    activeStyle={{background: 'pink'}}
+                >Login</NavLink>
+                <NavLink
+                    to="/about"
+                    exact
+                    style={link}
+                    activeStyle={{background: 'pink'}}
+                >About</NavLink>
+            </div>
+        )
+    }
+}
+
+export default NavBar
