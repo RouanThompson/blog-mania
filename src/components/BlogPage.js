f14b2c97f08ca7684de5812725ebcd04028268e6
Rouan Thompson <rouan.thompson@gmail.com>
2020-10-07 17:03:51 -0400

    Edit form is no longer shown within the orginal form. Edit form is only shown when edit button is clicked instead of showing both edit form and ordinal form

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -5,7 +5,7 @@ import MakeComment from './MakeComment'
 
 
 class BlogPage extends React.Component {
-    
+
     state = {
         showEditForm: false,
         id: this.props.blog.id,
@@ -20,7 +20,7 @@ class BlogPage extends React.Component {
         console.log("blog ID", this.state.id)
         let { comments } = this.props.blog
         comments.reverse()
-        return comments.map(comment => <CommentCard key={comment.id} comment={comment} commentId={comment.id} blogId={this.props.blog.id} currentUser={this.props.currentUser} deleteCommentState={this.props.deleteCommentState} updateCommentState={this.props.updateCommentState}/>)
+        return comments.map(comment => <CommentCard key={comment.id} comment={comment} commentId={comment.id} blogId={this.props.blog.id} currentUser={this.props.currentUser} deleteCommentState={this.props.deleteCommentState} updateCommentState={this.props.updateCommentState} />)
     }
 
     //controlled inputs
@@ -37,7 +37,7 @@ class BlogPage extends React.Component {
         //render the edit form with previous values
 
         //prevents reload on click
-        if (event){
+        if (event) {
             event.preventDefault()
         }
         this.setState({
@@ -69,20 +69,20 @@ class BlogPage extends React.Component {
             .then(blogs => {
                 this.props.updateBlogState(blogs)
             })
-            this.toggleEditForm()
+        this.toggleEditForm()
     }
 
     handleEdit = () => {
 
         // let banner = "./think-2.png"
         return (
-                // <img src={banner} className="banner2" alt=""></img><br/>
+            // <img src={banner} className="banner2" alt=""></img><br/>
             <div>
                 <form onSubmit={this.formHandlePatch}>
                     <div>
                         <div>
                             {/* <label>Title </label> */}
-                            <input className="formTitle" type="text"  name="title" placeholder="Title" value={this.state.title} onChange={this.formHandleChange}></input>
+                            <input className="formTitle" type="text" name="title" placeholder="Title" value={this.state.title} onChange={this.formHandleChange}></input>
                         </div>
                         {/* <input className="formTitle" type="file"></input> */}
                         <div>
@@ -107,42 +107,46 @@ class BlogPage extends React.Component {
         })
         //uses a callback function that takes the blog id
         this.props.deleteBlogState(this.props.blog.id)
-    } 
+    }
 
 
     // use link component from react router
-    render(){
+    render() {
         // console.log("In Blog Card", this.props.currentUser.userId)
-        let {blog} = this.props
-        return(
-            <div className="blogStory">
-                    <div>
-                        {blog.user.id === this.props.currentUser.userId
-                            ? <button onClick={this.toggleEditForm}>Edit</button>
-                            : null
-                        }
-                        {blog.user.id === this.props.currentUser.userId
-                            ? <button onClick={this.handleDelete}>Delete</button>
-                            : null
-                        }
+        let { blog } = this.props
+        return (
+            <div>
+                {this.state.showEditForm
+                    ? this.handleEdit()
+                    : <div className="blogStory">
+                        <div>
+                            {blog.user.id === this.props.currentUser.userId
+                                ? <button onClick={this.toggleEditForm}>Edit</button>
+                                : null
+                            }
+                            {blog.user.id === this.props.currentUser.userId
+                                ? <button onClick={this.handleDelete}>Delete</button>
+                                : null
+                            }
+                        </div>
+                        <h3>{blog.title}</h3>
+                        {/* <h4>{blog.cover_image}</h4> */}
+                        <h4>by {blog.user.name}</h4>
+                        <p className="story">{blog.story}</p>
+                        {/* <h5>likes {blog.likes}</h5> */}
+                        <br />
+                        <div>
+                            <h4>Comments</h4>
+                            <div>
+                                <MakeComment blogId={blog.id} currentUser={this.props.currentUser} newCommentState={this.props.newCommentState} />
+                            </div>
+                            <div>
+                                {this.renderComments()}
+                            </div>
+                        </div>
+                        <br/>
                     </div>
-                    <h3>{blog.title}</h3>
-                    {/* <h4>{blog.cover_image}</h4> */}
-                    <h4>by {blog.user.name}</h4>
-                    <p className="story">{blog.story}</p>
-                    {/* <h5>likes {blog.likes}</h5> */}
-                    {this.state.showEditForm ? this.handleEdit() : null}
-                <br/>
-                <div>
-                    <h4>Comments</h4>
-                    <div>
-                        <MakeComment blogId={blog.id} currentUser={this.props.currentUser} newCommentState={this.props.newCommentState}/>
-                    </div>
-                    <div>
-                        {this.renderComments()}
-                    </div>
-                </div>
-                <br/>
+                }
             </div>
         )
     }
